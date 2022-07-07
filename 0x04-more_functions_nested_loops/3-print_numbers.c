6846ce71ccfb6e1fe760f93322098fb0034ad1d5
Seid Muhammed <abuhaithemalthry@gmail.com>
2022-07-07 22:49:09 +0100

    my mandatory Projects last update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
-3-print_numbers.c
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers from 1 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar (n + '0');
+	}
+		_putchar ('\n');
+}
