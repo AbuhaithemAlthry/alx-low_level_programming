e370dd54a70eb28b395881664886186b17f7c4c0
Seid Muhammed <abuhaithemalthry@gmail.com>
2022-07-22 17:02:49 -0700

    final work

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,18 @@
-trlen.c
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: length.
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,14 @@
-kj-puts.c
+#include "main.h"
+
+/**
+ * _puts - Prints a string .
+ * @str: string.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
+
