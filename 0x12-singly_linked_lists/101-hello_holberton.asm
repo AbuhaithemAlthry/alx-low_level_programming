730b0a9983463e18c09221810ce4c69973de470f
Seid Muhammed <abuhaithemalthry@gmail.com>
2022-08-11 22:45:51 +0300

    once and for all my final commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,13 @@
-section .data
-hello:	db "Hello, Holberton",10,0
-format:	db "%s",0
+	global 	main
+	extern 	printf
 
-    section .text
-    global main
-    extern printf
-
+	section .text
 main:
-    push rbp
-    mov rbp, rsp
-
-    mov rsi, hello
-    mov rdi, format
-    call printf
-
-    pop rbp
-    mov rax, 0
-    ret
-
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
     
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -23,12 +20,11 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char);
-int _strlen(const char  *head);
-size_t print_list(const list_t  *h);
-size_t list_len(const list_t  *h);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+#endif /* _LISTS_H_ */
 
-#endif
